180af05f646432da5594a33c0926ebdc4c1a0cef
duythinh26 <pduythinh79@gmail.com>
2025-06-18 16:30:18 +0700

    create navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Movies from './pages/Movies.jsx'
+import MovieDetails from './pages/MovieDetails.jsx'
+import SeatLayout from './pages/SeatLayout.jsx'
+import Bookings from './pages/Bookings.jsx'
+import Favorite from './pages/Favorite.jsx'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
+
+    const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
     return (
-        <div>App</div>
+        <>
+            <Toaster />
+            {!isAdminRoute &&<Navbar />}
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/movies" element={<Movies />}/>
+                <Route path="/movies/:id" element={<MovieDetails />}/>
+                <Route path="/movies/:id/:date" element={<SeatLayout />}/>
+                <Route path="/booking" element={<Bookings />}/>
+                <Route path="/favorite" element={<Favorite />}/>
+            </Routes>
+            {!isAdminRoute &&<Footer />}
+        </>
     )
 }
 
