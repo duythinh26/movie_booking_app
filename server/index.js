849d398c45f4a32edba42c9032fd901462f96dfb
duythinh26 <pduythinh79@gmail.com>
2025-07-15 13:35:01 +0700

    initialize server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+await connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware())
+
+// API Routes
+app.get('/', (req, res) => {
+    res.send('Live server is running');
+})
+
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
