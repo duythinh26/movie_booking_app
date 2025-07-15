ec33b0628360bbf816006fa86a935e646c430a7d
duythinh26 <pduythinh79@gmail.com>
2025-07-15 20:52:57 +0700

    initialize show routes for admin dashboard

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import showRouter from './routes/showRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,13 +18,14 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware())
 
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 // API Routes
 app.get('/', (req, res) => {
     res.send('Live server is running');
 })
-
-// Set up the "/api/inngest" (recommended) routes with the serve handler
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/show', showRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
